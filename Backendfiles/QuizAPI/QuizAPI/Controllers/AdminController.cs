a21efea8d5b7df26b0b2b275e8bc96806be3cf2e
KaranyiczGabor <karanyiczg@kkszki.hu>
2025-03-02 10:24:39 +0100

    4 user related admin endpoint is done.

--- Backendfiles/QuizAPI/QuizAPI/Controllers/AdminController.cs
+++ Backendfiles/QuizAPI/QuizAPI/Controllers/AdminController.cs
@@ -1,25 +1,88 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QuizAPI.Models;
+using QuizAPI.Services.Dtos;
 using QuizAPI.Services.IService;
 
 namespace QuizAPI.Controllers
 {
     [Route("api/admin")]
     [ApiController]
+    [Authorize(Roles = "Admin")]
     public class AdminController : ControllerBase
     {
-        private readonly IAuthService _auth;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public AdminController(IAuthService auth)
+        public AdminController(UserManager<ApplicationUser> userManager)
         {
-            _auth = auth;
+            _userManager = userManager;
         }
-        [Authorize(Roles ="Admin")]
-        [HttpGet("GetUserAdmin")]
-        public async Task<ActionResult> GetUserAdmin()
-        {
 
+        [HttpGet("GetUsers")]
+        public async Task<ActionResult<IEnumerable<ApplicationUser>>> GetUsers()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            if (users != null)
+            {
+                return Ok(users);
+            }
+            return BadRequest();
+        }
+
+        [HttpGet("GetUsersbyId")]
+        public async Task<ActionResult<ApplicationUser>> GetUsersbyId(string Id)
+        {
+            var user = await _userManager.FindByIdAsync(Id);
+            if (user == null)
+            {
+                return NotFound("A keresett felhasznalo nem letezik");
+            }
+            return Ok(user);
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateUser(string id, [FromBody] AdminDto model)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+
+            if (user == null)
+            {
+                return NotFound("A keresett felhasznalo nem letezik.");
+            }
+
+            user.UserName = model.UserName;
+            user.Email = model.Email;
+
+            var result = await _userManager.UpdateAsync(user);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+
+            return Ok(new { Message = "A felhasznalo sikeresen frissitve." });
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteUser(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+
+            if (user == null)
+            {
+                return NotFound("A keresett felhasznalo nem letezik.");
+            }
+
+            var result = await _userManager.DeleteAsync(user);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+
+            return Ok(new { Message = "A felhasznalo torolve lett!" });
         }
     }
 }
--- Backendfiles/QuizAPI/QuizAPI/obj/Debug/net8.0/QuizAPI.AssemblyInfo.cs
+++ Backendfiles/QuizAPI/QuizAPI/obj/Debug/net8.0/QuizAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea88c8d321052a1697f47badc6cf55bdf18e51cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c63750520f95f254299e0efe2095fa2a3f1ceaac")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
