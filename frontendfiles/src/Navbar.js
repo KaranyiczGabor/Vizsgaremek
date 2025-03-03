c078935071b764e08e0fc018d5cde0c86424ab09
PeremiczkyI <peremiczkyi@kkszki.hu>
2025-03-03 08:35:03 +0100

    Navbar fixelve

--- frontendfiles/src/Navbar.js
+++ frontendfiles/src/Navbar.js
@@ -19,7 +19,7 @@ export default function Navbar() {
 
   return (
     <>
-      <nav className="navbar">
+      <nav className="nbar">
         <div className="navbar-left">
           <button
             className={`menu-button ${isOpen ? "open" : ""}`}
