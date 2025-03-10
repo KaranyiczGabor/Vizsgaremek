ea03bc25aab99e97047185b26fafa6130e48e7f5
BiroMark <birom@kkszki.hu>
2025-03-10 13:15:03 +0100

    admin komponensek létrehozása

---
+++ frontendfiles/src/Adminquestion.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Adminquestion() {
+  return (
+    <div style={{padding:20}}>
+      adydsadadad
+    </div>
+  )
+}
---
+++ frontendfiles/src/Adminuser.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Adminuser() {
+  return (
+    <div style={{padding:50}}>
+      asfdfaafdfssff
+    </div>
+  )
+}
