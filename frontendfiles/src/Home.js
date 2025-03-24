629b2b3436d3274fc26f051d2a982cb87be09432
PeremiczkyI <peremiczkyi@kkszki.hu>
2025-03-24 11:56:37 +0100

    Sorrend változtatva

--- frontendfiles/src/Home.js
+++ frontendfiles/src/Home.js
@@ -4,7 +4,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import Footer from './Footer';
 export default function Home() {
   const navigate = useNavigate();
-  const categories = ["Sport", "Történelem", "Földrajz", "Matematika", "Irodalom"];
+  const categories = ["Történelem", "Földrajz", "Matematika", "Sport", "Irodalom"];
 
   return (
     <div>
